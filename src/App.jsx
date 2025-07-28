19aa7c5749e38bdec3a2152572d8c85bd959218b
jsah-mc <sah.joseph@outlook.com>
2025-07-28 17:55:59 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useRef, useEffect } from "react";
+import "./App.css";
+import Typed from "typed.js";
+
+function App() {
+  const el = useRef(null);
+  const typed = useRef(null);
+
+  useEffect(() => {
+    typed.current = new Typed(el.current, {
+      strings: ["Hello, World!", "Welcome to my website!", "Enjoy your stay!"],
+      typeSpeed: 100,
+      backSpeed: 0,
+      loop: true,
+      showCursor: false,
+      cursorChar: " ",
+    });
+
+    // Cleanup Typed instance on unmount
+    return () => {
+      typed.current.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>
+        <span ref={el}></span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
